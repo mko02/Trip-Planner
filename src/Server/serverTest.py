95e4b0c799c660c249f1d1bb80ae801138f50ac2
Anderson <andersonelee0618@gmail.com>
2022-06-19 11:52:57 +0800

    Delete unnecessary files

--- src/Server/serverTest.py
+++ src/Server/serverTest.py
@@ -1,9 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
-'''
-    Still trying to figure out how to build a server in Python
-'''
 class Server(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
@@ -12,14 +9,14 @@ class Server(BaseHTTPRequestHandler):
         self.wfile.write(bytes(json.dumps({'text': 'Hello World'}), 'utf-8'))
 
 def main():
-    webServer = HTTPServer(('localhost', 8080), Server)
+    web_server = HTTPServer(('localhost', 8080), Server)
     print("local server started at localhost port 8080")
     try:
-        webServer.serve_forever()
+        web_server.serve_forever()
     except KeyboardInterrupt:
         pass
 
-    webServer.server_close()
+    web_server.server_close()
     print("Server stopped.")
 
 if __name__ == "__main__":
