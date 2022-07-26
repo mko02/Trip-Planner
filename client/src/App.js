dba1e94acfaec95e29efffddedf742ba1ee68765
Juliaaaachu <juliachu910125@gmail.com>
2022-07-27 00:28:09 +0800

    i dont remember what i changed

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route} from "react-router-dom";
 import Home from "./Pages/Home"
 import TransactionSheet from "./Pages/TransactionSheet";
 import Trip from "./Pages/Trip";
+import AddTrip from "./Pages/AddTrip"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/> 
         <Route path="/currenttrip" element={<Trip />}/> 
+        <Route path="/addtrip" element={<AddTrip />}/> 
         <Route path="/transactionsheet" element={<TransactionSheet />}/>
       </Routes>
     </div>
