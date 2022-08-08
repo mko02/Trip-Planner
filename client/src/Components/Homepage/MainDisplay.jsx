b6423426e74d949719bffab7f5ad49123ad41737
Juliaaaachu <juliachu910125@gmail.com>
2022-08-08 22:31:32 +0800

    idk what changed

--- client/src/Components/Homepage/MainDisplay.jsx
+++ client/src/Components/Homepage/MainDisplay.jsx
@@ -1,23 +1,20 @@
-import React, {useState} from 'react'
-import Heading from './Heading'
-import {Box} from '@mui/material'
-import TripList from './TripList/TripList';
+import React, { useState } from "react";
+import Heading from "./Heading";
+import { Box } from "@mui/material";
+import TripList from "./TripList/TripList";
 
-import {sample_package_data} from '../../DummyInfo/sample_package_data'
-
+import { sample_package_data } from "../../DummyInfo/sample_package_data";
 
 function Main() {
-  const [trips] = useState(sample_package_data)
+  const [trips] = useState(sample_package_data);
 
   return (
     <Box bgcolor="white" padding={2} flex={2}>
-      
-      <Heading/>
+      <Heading />
 
-      <TripList trips={trips}/>
-
+      <TripList trips={trips} />
     </Box>
-  )
+  );
 }
 
-export default Main
+export default Main;
